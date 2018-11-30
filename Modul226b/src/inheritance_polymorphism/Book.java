bf801b40b94d9b008f9a63b0f126f4ebc3409eb6
JoelRauber <joelarauber@hotmail.com>
2018-11-30 09:28:32 +0100

    ADDED: Startvorlage für Kompetenz A

---
+++ Modul226b/src/inheritance_polymorphism/Book.java
@@ -0,0 +1,11 @@
+package inheritance_polymorphism;
+
+public class Book {
+	private String title;
+	private String author;
+	private String isbn;
+	private int pages;
+	private boolean gotIt;
+	private String comment;
+
+}
